830368bc73d5fb5377c24aecd98fa762b2bdce39
Mun Jisu <answltn8@gmail.com>
2023-09-26 14:42:56 +0900

    [create Hashmap dic program] Test Hashmap function(search, print)

---
+++ javaTest/src/ch07/sec04/HashMapTest.java
@@ -0,0 +1,45 @@
+package ch07.sec04;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class HashMapTest {
+	public static Scanner scan = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		HashMap<String, String> dic = new HashMap<String, String>();
+		
+		// HashMap 추가
+		dic.put("baby", "아기");
+		dic.put("love", "사랑");
+		dic.put("apple", "사과");
+		
+		// HashMap 출력
+		
+		for(String key:dic.keySet()) {
+			System.out.println(key + ": " + dic.get(key));
+		}
+		System.out.println();
+		
+		// for each문으로 출력
+		dic.forEach((key,value) -> System.out.println(key +": " + value));
+		System.out.println("==============================================");
+		
+		//검색기능
+		while(true) {
+			System.out.print("검색단어(종료:exit)>>");
+			String search = scan.nextLine().trim();
+			if("exit".equals(search) || "EXIT".equals(search)) {
+				System.out.println("단어 검색을종료합니다.");
+				break;
+			}
+			System.out.println(search + "는 " + (dic.get(search) != null ? dic.get(search) : "값이 없습니다.")); 
+		}
+		
+		System.out.println("The end");
+		
+		
+	}
+
+}
